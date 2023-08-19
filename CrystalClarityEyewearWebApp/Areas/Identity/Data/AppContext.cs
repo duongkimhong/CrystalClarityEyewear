dc8e9fd94ba32ac6c2266db99b7225c57810b8e5
duongkimhong <kimhongduong2109@gmail.com>
2023-08-19 13:18:35 +0700

    Create App MVC

---
+++ CrystalClarityEyewearWebApp/Areas/Identity/Data/AppContext.cs
@@ -0,0 +1,31 @@
+﻿using CrystalClarityEyewearWebApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrystalClarityEyewearWebApp.Areas.Identity.Data;
+
+public class AppContext : IdentityDbContext<ApplicationUser>
+{
+    public AppContext(DbContextOptions<AppContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            var tableName = entityType.GetTableName();
+            if (tableName.StartsWith("AspNet"))
+            {
+                entityType.SetTableName(tableName.Substring(6));
+            }
+        }
+    }
+}
---
+++ CrystalClarityEyewearWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrystalClarityEyewearWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
